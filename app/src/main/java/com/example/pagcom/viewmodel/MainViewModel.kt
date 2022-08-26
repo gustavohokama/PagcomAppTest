4824e7e3374f010b1a26e8564d5768c4327df319
gustavohokama <gustavohokama@gmail.com>
2022-08-26 19:53:17 -0300

    branch to development

--- app/src/main/java/com/example/pagcom/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/pagcom/viewmodel/MainViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.pagcom.viewmodel
 
 import android.Manifest
+import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
 import android.provider.MediaStore
 import android.util.Log
@@ -17,16 +20,19 @@ import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavController
 import com.example.pagcom.web.CompanieWebClient
 import com.example.pagcom.web.model.CompaniesResponse
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
 
     private lateinit var navController: NavController
     private lateinit var context: AppCompatActivity
-    var bitmap: MutableLiveData<Bitmap> = MutableLiveData<Bitmap>()
-    var regisName: MutableLiveData<String> = MutableLiveData<String>()
-    var regisTel: MutableLiveData<String> = MutableLiveData<String>()
-    var regisCPF: MutableLiveData<String> = MutableLiveData<String>()
+    val bitmap: MutableLiveData<Bitmap> = MutableLiveData<Bitmap>()
+    val regisName: MutableLiveData<String> = MutableLiveData<String>()
+    val regisTel: MutableLiveData<String> = MutableLiveData<String>()
+    val regisCPF: MutableLiveData<String> = MutableLiveData<String>()
+    val resposta: MutableLiveData<String> = MutableLiveData<String>()
+
 
     fun firstConfig(context: AppCompatActivity) {
         this.context = context
@@ -34,19 +40,59 @@ class MainViewModel : ViewModel() {
     }
 
     private fun checkPermissions() {
-        val checkSelfPermission =
-            ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.INTERNET
-            ) == PackageManager.PERMISSION_GRANTED
-
-        if (!checkSelfPermission) {
+        if (!internetPermission()) {
             ActivityCompat.shouldShowRequestPermissionRationale(
                 context,
                 Manifest.permission.INTERNET
             )
         }
+
+        if (!cameraPermission()) {
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                context,
+                Manifest.permission.CAMERA
+            )
+        }
+
+        if (!locationPerminssion()){
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                context,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            ActivityCompat.shouldShowRequestPermissionRationale(
+                context,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        }
+
     }
+
+    private fun internetPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.INTERNET
+        ) == PackageManager.PERMISSION_GRANTED
+
+    }
+
+    private fun cameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun locationPerminssion(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+    }
+
 
     fun listCompanies(callback: (List<CompaniesResponse>?) -> Unit) {
         viewModelScope.launch {
@@ -68,21 +114,17 @@ class MainViewModel : ViewModel() {
             regisTel.value.isNullOrBlank() ||
             regisCPF.value.isNullOrBlank()
         ) {
-            Toast.makeText( context, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
-        }else{
+            Toast.makeText(context, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+        } else {
             regisName.value = ""
             regisTel.value = ""
             regisCPF.value = ""
-            Toast.makeText( context, "Usuário Cadastrado com Sucesso", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context, "Usuário Cadastrado com Sucesso", Toast.LENGTH_SHORT).show()
         }
     }
 
     fun getLocation() {
-        if(Build.MODEL == "A910"){
 
-        }else{
-
-        }
     }
 
     fun setImage(bitmap: Bitmap) {
